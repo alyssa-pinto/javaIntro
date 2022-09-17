b44b3f49110af2ad9177e8e17de3ae0d92e7ec0f
alyssa pinto <alyssapinto05@gmail.com>
2022-09-17 17:24:37 -0400

    This is my first girls who code into lesson

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        isLysHere(false);
+        isLysHere(true);
+    }
+
+    private static void isLysHere(boolean isAlyssaHere) {
+        if(isAlyssaHere){
+            System.out.println("Hello world!, I'm here");
+        }
+        else{
+            System.out.println("I'm not here");
+        }
+    }
+}
